7cdd34e9a5b62ef7e3bd1681820824b41e01e8ec
Owned67 <qx9650@hotmail.fr>
2025-02-10 11:03:05 +0100

    v0.18.4
    v0.18.4

--- MonumentAddons.cs
+++ MonumentAddons.cs
@@ -46,7 +46,7 @@ using Tuple4 = System.ValueTuple<object, object, object, object>;
 
 namespace Oxide.Plugins
 {
-    [Info("Monument Addons", "WhiteThunder", "0.18.3")]
+    [Info("Monument Addons", "WhiteThunder", "0.18.4")]
     [Description("Allows adding entities, spawn points and more to monuments.")]
     internal class MonumentAddons : CovalencePlugin
     {
@@ -5206,8 +5206,9 @@ namespace Oxide.Plugins
                         ? session.StartSlot.handlePosition
                         : session.StartSlot.linePoints.LastOrDefault();
 
-                    drawer.Sphere(hitPosition, PreviewDotRadius);
-                    drawer.Line(ioEntity.transform.TransformPoint(lastPoint), hitPosition);
+                    var lineDrawer = new Ddraw(player, DrawIntervalWithBuffer, Color.green);
+                    lineDrawer.Sphere(hitPosition, PreviewDotRadius);
+                    lineDrawer.Line(ioEntity.transform.TransformPoint(lastPoint), hitPosition);
                 }
             }
 
@@ -8877,7 +8878,11 @@ namespace Oxide.Plugins
                 DelayedSpawn();
             }
 
+            #if OXIDE_PUBLICIZED
+            public override void Spawn(int numToSpawn)
+            #else
             protected override void Spawn(int numToSpawn)
+            #endif
             {
                 numToSpawn = Mathf.Min(numToSpawn, maxPopulation - currentPopulation);
 
@@ -8923,7 +8928,11 @@ namespace Oxide.Plugins
                 }
             }
 
+            #if OXIDE_PUBLICIZED
+            public override void PostSpawnProcess(BaseEntity entity, BaseSpawnPoint spawnPoint)
+            #else
             protected override void PostSpawnProcess(BaseEntity entity, BaseSpawnPoint spawnPoint)
+            #endif
             {
                 base.PostSpawnProcess(entity, spawnPoint);
 
